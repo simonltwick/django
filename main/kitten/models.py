4f2cefd3cacef65b211bc868d7da0c8c4e899653
Simon Lambourn <simon.lambourn@gmail.com>
2020-04-26 09:32:55 +0100

    Updates to data model reflecting template games, template lines etc
    Games, Lines, places are now instantiated by copying from Network,
    GameTemplate, LineTemplate, and LocationType rather than being linked to
    them in the database.

--- main/kitten/models.py
+++ main/kitten/models.py
@@ -1,7 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import User
 from enum import IntEnum
+from typing import List
 
 
+class Team(models.Model):
+    LEVELS = ((10, 'Basic'), (20, 'Intermediate'),
+              (30, 'Advanced'), (40, 'Expert'))
+    name = models.CharField(max_length=40)
+    members = models.ManyToManyField(User, related_name='teams')
+    owner = models.ForeignKey(User, on_delete=models.PROTECT,
+                              related_name='teams_owned')
+    # games = reverse FK
+    level = models.IntegerField(choices=LEVELS)
+
+    def __str__(self):
+        return self.name
+
+
+# ------ network and game templating classes
 class Network(models.Model):
     # map
     # font
@@ -10,7 +27,15 @@ class Network(models.Model):
     description = models.CharField(max_length=300)
     created = models.DateField(auto_now_add=True)
     last_saved = models.DateField(auto_now=True)
-    # owner = models.ForeignKey(User, on_delete=models.WARN)
+    owner = models.ForeignKey(User, on_delete=models.PROTECT, null=True)
+    # lines = reverse FK (to LineTemplate)
+    # game templates= reverse FK
+    # incident_types= reverse FK
+    # response_types= reverse FK
+    # response_options= reverse FK
+    # impacts = reverse FK
+    # impact_types = reverse FK
+    # passenter_profile
     # settings
     # min. level to play & success criteria & completion score
     # last use
@@ -19,30 +44,91 @@ class Network(models.Model):
         return self.name
 
 
-class Team(models.Model):
+class LineTemplate(models.Model):
     name = models.CharField(max_length=40)
-    # -> users
-    # levels_unlocked (some kind of score)
+    network = models.ForeignKey(Network, related_name='lines',
+                                on_delete=models.CASCADE)
 
     def __str__(self):
-        return self.name
+        return f'{self.name} in {self.network}'
 
 
+class PlaceTemplate(models.Model):
+    name = models.CharField(
+        max_length=40, help_text="A name is only required for stations",
+        null=True, verbose_name="Name (if station)", blank=True)
+    position = models.IntegerField()
+    line = models.ForeignKey(LineTemplate, related_name='places',
+                             on_delete=models.CASCADE)
+    transit_delay = models.IntegerField(
+        default=1, help_text='Wait time at stations or depots')
+    # TODO: validate that station names are non-blank
+
+    class LocationType(IntEnum):
+        # NB must tie up with LocationType in Places, below
+        DEPOT = 0
+        TRACK = 1
+        STATION = 2
+
+    location_type = models.IntegerField(
+        default=1, choices=[(0, 'Depot'), (1, 'Track'), (2, 'Station')])
+
+    class Meta:
+        unique_together = (('line', 'position'))
+
+    def display_name(self):
+        return (self.name if self.name else
+                f'[{self.get_location_type_display()}]')
+
+    def __str__(self):
+        return self.display_name()
+
+
+class GameTemplate(models.Model):
+    level = models.IntegerField(choices=Team.LEVELS)
+    network = models.ForeignKey(Network, related_name='levels',
+                                on_delete=models.CASCADE)
+
+    class Meta:
+        unique_together = (('network', 'level'))
+
+    def __str__(self):
+        return f'{self.get_level_display()} game on {self.network}'
+
+
+# ----- Game related models ------
 class Game(models.Model):
     # level
-    name = models.CharField(max_length=40, null=True)
+    name = models.CharField(max_length=40, null=True, blank=True)
     started = models.DateField(auto_now_add=True)
     last_played = models.DateField(auto_now=True)
-    team = models.ManyToManyField(Team, related_name='games')
-    network = models.ForeignKey(Network, on_delete=models.PROTECT)
+    teams = models.ManyToManyField(Team, related_name='games')
+    network_name = models.CharField(max_length=40)
+    level = models.IntegerField(choices=Team.LEVELS)
 
     def __str__(self):
         return f"{self.name if self.name else ''} started {self.started}, "\
             f"last played {self.last_played}"
 
+    @classmethod
+    def new_from_template(cls, template: GameTemplate, teams: List[Team],
+                          **kwargs):
+        """ make a new game from GameTemplate, including
+        Lines from game_template.line.line_templates
+        LineLocations and Tracks from line_templates.place_templates
+        Trains from LineTemplates
+        Only one team is required to start a game"""
+        network_name = template.network.name
+        level = template.level
+        game = Game.objects.create(network_name=network_name, level=level,
+                                   **kwargs)
+        for team in teams:
+            game.teams.add(team)
+        return game
 
+
+# ----- Line related models -----
 class Line(models.Model):
-    network = models.ForeignKey(Network)
     name = models.CharField(max_length=40)
     direction1 = models.CharField(max_length=20)
     direction2 = models.CharField(max_length=20)
@@ -59,14 +145,12 @@ class Line(models.Model):
 
 class GameLineParameters(models.Model):
     """ operational line parameter overrides within a game """
-    network = models.ForeignKey(Network, null=True)
-    game = models.ForeignKey(Game, on_delete=models.CASCADE)
+    game = models.ForeignKey(Game, on_delete=models.CASCADE, null=True)
     team = models.ForeignKey(Team, null=True)
-    name = models.CharField(max_length=40)
     trains_dir1 = models.IntegerField(default=3)
     trains_dir2 = models.IntegerField(default=3)
     train_frequency = models.IntegerField(default=10)
-    reputation = models.IntegerField(default=100)
+    line_reputation = models.IntegerField(default=100)
 
     def __str__(self):
         return str(self.name)
@@ -77,7 +161,10 @@ class GameLineParameters(models.Model):
 
 class LineLocation(models.Model):
     """ a place on the line: a station, a depot or a line  """
-    game = models.ForeignKey(GameLineParameters, on_delete=models.CASCADE)
+    game = models.ForeignKey(GameLineParameters, on_delete=models.CASCADE,
+                             null=True)
+    line = models.ForeignKey(Line, on_delete=models.CASCADE, null=True,
+                             related_name='locations')
     name = models.CharField(max_length=40)
     sequence = models.IntegerField()
     transit_time = models.IntegerField(default=1)
