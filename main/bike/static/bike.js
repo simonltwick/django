eebd4c2500e3eeba3e23a630e2f6713a25f8b458
Simon Lambourn <simon.lambourn@gmail.com>
2021-07-24 22:31:44 +0100

    Add Delete button which marks link to be deleted.

--- main/bike/static/bike.js
+++ main/bike/static/bike.js
@@ -8,3 +8,20 @@ function change_link_visibility(id) {
 	link = $('tr#link_' + id).find('td.link-link').addClass('hidden');
 	form = $('tr#link_' + id).find('td.link-form').removeClass('hidden');
 }
+
+function mark_link_deleted(id) {
+	/* toggle: mark form deleted and set strikethrough */
+
+	// <input> for deleted has id id_links-0-DELETE vs passed id=id_links-0-id
+	delete_id = id.slice(0, -3) + "-DELETE";
+	deleted_old = $('#' + delete_id).prop("checked");
+	deleted = !deleted_old;
+	console.info("link", id, "deleted_old=", deleted_old, "deleted =", deleted);
+	$('#' + delete_id).prop("checked", deleted);
+	
+	// find the link itself and set strikethrough
+	link = $('tr#link_' + id).find('a')[0];
+	link.style.textDecoration = deleted ? "line-through": "underline";
+	console.info("results: input.checked=", $('#' + delete_id).prop("checked"),
+		"link.style.textDecoration=", link.style.textDecoration);
+}
