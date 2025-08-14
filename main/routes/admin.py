6c91c58f511f7857ebecf1d0072deb2392ffb27f
Simon Lambourn <simon.lambourn@gmail.com>
2025-08-14 11:29:49 +0100

    Add route tags and placetypes to the admin menu

--- main/routes/admin.py
+++ main/routes/admin.py
@@ -1,5 +1,5 @@
 
-from routes.models import Place, Track
+from routes.models import Place, Track, PlaceType, Tag
 
 from django.contrib.gis import admin
 
@@ -8,11 +8,27 @@ from django.contrib.gis import admin
 #     # for earlier django release support (GISModelAdmin in 5.2)
 #     admin.GISModelAdmin = admin.GeoModelAdmin
 
+
 @admin.register(Place)
 class PlaceAdmin(admin.GISModelAdmin):
+    readonly_fields=('user',)
     list_display = ("name", "location")
+
 
 @admin.register(Track)
 class TrackAdmin(admin.GISModelAdmin):
+    readonly_fields=('user',)
     list_display = ("name", "pk")  #, "track"
     #3d multilinestrings not supported, so just displays textarea
+
+
+@admin.register(PlaceType)
+class PlaceTypeAdmin(admin.ModelAdmin):
+    readonly_fields=('user',)
+    fields=('name', 'icon')
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    readonly_fields=('user',)
+    fields=('name',)
